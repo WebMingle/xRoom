4d272df0e44b468cc9341cbfdb35d58d4c8f6d1e
Adrian Alvird <51330833+adrianalvird@users.noreply.github.com>
2023-11-18 10:08:43 +0530

    Update settings.py

--- securechat/settings.py
+++ securechat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yw5ogyudjr)_w1*!a(z=c*)!@&n6%+!vko*ro8%xdr73%)4vp$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'securechat.wsgi.application'
+WSGI_APPLICATION = 'securechat.wsgi.app'
 
 
 # Database
@@ -116,7 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
